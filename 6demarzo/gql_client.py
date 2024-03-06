267c8b73a29da7e184c1a2529e57a19eadd3d202
josemarcelopoma <153699804+josemarcelopoma@users.noreply.github.com>
2024-03-06 17:51:22 +0000

    clase6demarzo

---
+++ 6demarzo/gql_client.py
@@ -0,0 +1,19 @@
+import requests
+
+# Definir la consulta GraphQL
+query = """
+    {
+        estudiante{id,
+        nombre
+        apellido
+        carrera}
+    }
+
+"""
+
+# Definir la URL del servidor GraphQL
+url = 'http://localhost:8000/graphql'
+
+# Solicitud POST al servidor GraphQL
+response = requests.post(url, json={'query': query})
+print(response.text)
