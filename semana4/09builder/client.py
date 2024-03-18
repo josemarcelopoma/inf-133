ab8e47523a264fb1bcceebaecadeb657cf9abd9b
josemarcelopoma <153699804+josemarcelopoma@users.noreply.github.com>
2024-03-18 17:56:33 +0000

    clase18marzo

---
+++ semana4/09builder/client.py
@@ -0,0 +1,28 @@
+import requests
+
+url = "http://localhost:8000/pizza"
+headers = {'Content-type': 'application/json'}
+
+mi_pizza = {
+    "tamaño": "Grande",
+    "masa": "Delgada",
+    "toppings": ["Jamon", "Queso"]
+}
+response = requests.post(url, json=mi_pizza, headers=headers)
+print(response.json())
+
+mi_pizza = {
+    "tamaño": "Grande",
+    "masa": "Delgada",
+    "toppings": ["Jamon", "Queso"]
+}
+response = requests.post(url, json=mi_pizza, headers=headers)
+print(response.json())
+
+mi_pizza = {
+    "tamaño": "Grande",
+    "masa": "Delgada",
+    "toppings": ["Jamon", "Queso"]
+}
+response = requests.post(url, json=mi_pizza, headers=headers)
+print(response.json())
